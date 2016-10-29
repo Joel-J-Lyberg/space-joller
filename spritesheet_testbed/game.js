d156a6e90924681f950b05722697506e14cf79db
Erik Larsson <erik.mattias.larsson@gmail.com>
2016-10-29 15:26:04 +0200

    More graphics

--- spritesheet_testbed/game.js
+++ spritesheet_testbed/game.js
@@ -4,6 +4,12 @@ var context = canvas.getContext("2d")
 var invader = new Image();
 invader.src = "../graphics/enemy1_spritesheet.png";
 
+var player = new Image();
+player.src = "../graphics/player.png";
+
+var player_exploding = new Image();
+player_exploding.src = "../graphics/player_explosion_spritesheet.png";
+
 var invader_exploding = new Image();
 invader_exploding.src = "../graphics/enemy_exploding_spritesheet.png";
 
@@ -53,11 +59,22 @@ var star_spritesheet2 = SpriteSheet.new(star, {
   autostart: true,
 })
 
+var player_exploding_spritesheet = SpriteSheet.new(player_exploding, {
+  frames: [1000, 100, 200, 300, 400],
+  x: 0,
+  y: 0,
+  width: 42,
+  height: 42,
+  restart: true,
+  autostart: true,
+})
+
 star_spritesheet.play();
 star_spritesheet2.play();
 star_spritesheet2.tick(2000);
 invader_spritesheet.play();
 invader_exploding_spritesheet.play();
+player_exploding_spritesheet.play();
     
 setInterval(function() {
     context.drawImage(background, 0,0)
@@ -84,5 +101,16 @@ setInterval(function() {
     context.translate(50, 200);
     star_spritesheet2.tick(1000/60);
     star_spritesheet2.draw(context);
+    context.restore();
+
+    context.save()
+    context.translate(200, 500);
+    context.drawImage(player, 0,0 );
+    context.restore();
+
+    context.save()
+    context.translate(200, 100);
+    player_exploding_spritesheet.tick(1000/60);
+    player_exploding_spritesheet.draw(context);
     context.restore();
 }, 1000/60);
