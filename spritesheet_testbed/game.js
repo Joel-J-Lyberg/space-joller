e4b1f41082194c8d04c23076dedc2126d8a81934
Erik Larsson <erik.mattias.larsson@gmail.com>
2016-10-29 15:16:17 +0200

    Much graphics

--- spritesheet_testbed/game.js
+++ spritesheet_testbed/game.js
@@ -2,9 +2,18 @@ var canvas = document.getElementById("canvas")
 var context = canvas.getContext("2d")
 
 var invader = new Image();
-invader.src = "enemy1_spritesheet.png";
+invader.src = "../graphics/enemy1_spritesheet.png";
 
-var spritesheet = SpriteSheet.new(invader, {
+var invader_exploding = new Image();
+invader_exploding.src = "../graphics/enemy_exploding_spritesheet.png";
+
+var star = new Image();
+star.src = "../graphics/star_spritesheet.png";
+
+var background = new Image();
+background.src = "../graphics/background.png";
+
+var invader_spritesheet = SpriteSheet.new(invader, {
   frames: [200, 200, 200],
   x: 0,
   y: 0,
@@ -14,11 +23,66 @@ var spritesheet = SpriteSheet.new(invader, {
   autostart: true,
 })
 
-setInterval(function() {
-    context.fillStyle = "black";
-    context.fillRect(0,0, 800, 600);
+var invader_exploding_spritesheet = SpriteSheet.new(invader_exploding, {
+  frames: [1000, 50, 100, 100, 100, 150, 200],
+  x: 0,
+  y: 0,
+  width: 14 * 3,
+  height: 14 * 3,
+  restart: true,
+  autostart: true,
+})
 
-    spritesheet.play();
-    spritesheet.tick(1000/60);
-    spritesheet.draw(context);
+var star_spritesheet = SpriteSheet.new(star, {
+  frames: [3000, 50, 200, 200, 200, 50],
+  x: 0,
+  y: 0,
+  width: 21,
+  height: 21,
+  restart: true,
+  autostart: true,
+})
+
+var star_spritesheet2 = SpriteSheet.new(star, {
+  frames: [3000, 50, 200, 200, 200, 50],
+  x: 0,
+  y: 0,
+  width: 21,
+  height: 21,
+  restart: true,
+  autostart: true,
+})
+
+star_spritesheet.play();
+star_spritesheet2.play();
+star_spritesheet2.tick(2000);
+invader_spritesheet.play();
+invader_exploding_spritesheet.play();
+    
+setInterval(function() {
+    context.drawImage(background, 0,0)
+
+    context.save()
+    context.translate(100, 100);
+    invader_spritesheet.tick(1000/60);
+    invader_spritesheet.draw(context);
+    context.restore();
+
+    context.save()
+    context.translate(300, 100);
+    invader_exploding_spritesheet.tick(1000/60);
+    invader_exploding_spritesheet.draw(context);
+    context.restore();
+
+    context.save()
+    context.translate(200, 400);
+    star_spritesheet.tick(1000/60);
+    star_spritesheet.draw(context);
+    context.restore();
+
+    context.save()
+    context.translate(50, 200);
+    star_spritesheet2.tick(1000/60);
+    star_spritesheet2.draw(context);
+    context.restore();
 }, 1000/60);
